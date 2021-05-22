d35c78be42bff04e57e4b6b872f75201c9037aa2
ATZIN NATANAEL PACHECO ARELLANO <Atzin.Pacheco@alumno.udg.mx>
2021-05-22 01:20:04 -0500

    Agregar archivos de proyecto.

---
+++ 5 REINAS/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace _5_REINAS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label3;
+            System.Windows.Forms.Label label4;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.inicio = new System.Windows.Forms.Button();
+            this.empezar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel = new System.Windows.Forms.PictureBox();
+            this.reina2 = new System.Windows.Forms.PictureBox();
+            this.reina3 = new System.Windows.Forms.PictureBox();
+            this.reina4 = new System.Windows.Forms.PictureBox();
+            this.reina5 = new System.Windows.Forms.PictureBox();
+            this.reina1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.panel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reina2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reina3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reina4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reina5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reina1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            label3.Cursor = System.Windows.Forms.Cursors.No;
+            label3.Font = new System.Drawing.Font("Cooper Black", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label3.Location = new System.Drawing.Point(12, 9);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(18, 529);
+            label3.TabIndex = 15;
+            label3.Text = "Pacheco Arellano Atzin  Natanael";
+            label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label4
+            // 
+            label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            label4.Cursor = System.Windows.Forms.Cursors.No;
+            label4.Font = new System.Drawing.Font("Cooper Black", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label4.Location = new System.Drawing.Point(65, 80);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(18, 400);
+            label4.TabIndex = 16;
+            label4.Text = "Seminario de  Algoritmia";
+            label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // inicio
+            // 
+            this.inicio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.inicio.Location = new System.Drawing.Point(649, 38);
+            this.inicio.Name = "inicio";
+            this.inicio.Size = new System.Drawing.Size(112, 51);
+            this.inicio.TabIndex = 5;
+            this.inicio.Text = "INICIO";
+            this.inicio.UseVisualStyleBackColor = false;
+            this.inicio.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // empezar
+            // 
+            this.empezar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.empezar.Location = new System.Drawing.Point(649, 110);
+            this.empezar.Name = "empezar";
+            this.empezar.Size = new System.Drawing.Size(112, 51);
+            this.empezar.TabIndex = 6;
+            this.empezar.Text = "EMPEZAR / PAUSAR";
+            this.empezar.UseVisualStyleBackColor = false;
+            this.empezar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(931, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 7;
+            // 
+            // panel
+            // 
+            this.panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel.Location = new System.Drawing.Point(120, 38);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(500, 500);
+            this.panel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.panel.TabIndex = 8;
+            this.panel.TabStop = false;
+            // 
+            // reina2
+            // 
+            this.reina2.Image = ((System.Drawing.Image)(resources.GetObject("reina2.Image")));
+            this.reina2.Location = new System.Drawing.Point(140, 155);
+            this.reina2.Name = "reina2";
+            this.reina2.Size = new System.Drawing.Size(56, 69);
+            this.reina2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.reina2.TabIndex = 9;
+            this.reina2.TabStop = false;
+            // 
+            // reina3
+            // 
+            this.reina3.Image = ((System.Drawing.Image)(resources.GetObject("reina3.Image")));
+            this.reina3.Location = new System.Drawing.Point(140, 257);
+            this.reina3.Name = "reina3";
+            this.reina3.Size = new System.Drawing.Size(56, 69);
+            this.reina3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.reina3.TabIndex = 10;
+            this.reina3.TabStop = false;
+            // 
+            // reina4
+            // 
+            this.reina4.Image = ((System.Drawing.Image)(resources.GetObject("reina4.Image")));
+            this.reina4.Location = new System.Drawing.Point(140, 350);
+            this.reina4.Name = "reina4";
+            this.reina4.Size = new System.Drawing.Size(56, 69);
+            this.reina4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.reina4.TabIndex = 11;
+            this.reina4.TabStop = false;
+            // 
+            // reina5
+            // 
+            this.reina5.Image = ((System.Drawing.Image)(resources.GetObject("reina5.Image")));
+            this.reina5.Location = new System.Drawing.Point(140, 451);
+            this.reina5.Name = "reina5";
+            this.reina5.Size = new System.Drawing.Size(56, 69);
+            this.reina5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.reina5.TabIndex = 12;
+            this.reina5.TabStop = false;
+            // 
+            // reina1
+            // 
+            this.reina1.Image = ((System.Drawing.Image)(resources.GetObject("reina1.Image")));
+            this.reina1.Location = new System.Drawing.Point(140, 58);
+            this.reina1.Name = "reina1";
+            this.reina1.Size = new System.Drawing.Size(56, 69);
+            this.reina1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.reina1.TabIndex = 13;
+            this.reina1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Font = new System.Drawing.Font("MS Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Location = new System.Drawing.Point(322, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 24);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "5 REINAS";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(785, 562);
+            this.Controls.Add(this.empezar);
+            this.Controls.Add(this.inicio);
+            this.Controls.Add(label4);
+            this.Controls.Add(label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.reina1);
+            this.Controls.Add(this.reina5);
+            this.Controls.Add(this.reina4);
+            this.Controls.Add(this.reina3);
+            this.Controls.Add(this.reina2);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.panel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reina2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reina3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reina4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reina5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reina1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button inicio;
+        private System.Windows.Forms.Button empezar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox panel;
+        private System.Windows.Forms.PictureBox reina2;
+        private System.Windows.Forms.PictureBox reina3;
+        private System.Windows.Forms.PictureBox reina4;
+        private System.Windows.Forms.PictureBox reina1;
+        public System.Windows.Forms.PictureBox reina5;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 5 REINAS/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//PACHECO ARELLANO ATZIN NATANAEL
+//SEMINARIO DE ALGORITMIA
+namespace _5_REINAS
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap bmp;
+
+        int r1 = 140;
+        int r2 = 140;
+        int r3 = 140;
+        int r4 = 140;
+        int r5 = 140;
+
+        int solucion = 1;
+        bool flag = false;
+        bool flag2 = false;
+        Thread hilo;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            Tablero();
+            hilo = new Thread (reinas);
+            CheckForIllegalCrossThreadCalls = false;
+            inicio.Enabled = false;
+
+
+        }
+       
+        
+
+
+        void Tablero()
+        {
+            try
+            {
+                Bitmap bmp = new Bitmap(Width, Height);
+                panel.Image = bmp;
+                this.reina1.SizeMode = PictureBoxSizeMode.Zoom;
+                this.reina2.SizeMode = PictureBoxSizeMode.Zoom;
+                this.reina3.SizeMode = PictureBoxSizeMode.Zoom;
+                this.reina4.SizeMode = PictureBoxSizeMode.Zoom;
+                this.reina5.SizeMode = PictureBoxSizeMode.Zoom;
+                int x, y;
+                Color c;
+
+                //DIBUJAR 
+                for (y = 0; y < bmp.Height; y++)
+                {
+                    for (x = 0; x < bmp.Width; x++)
+                    {
+                        c = bmp.GetPixel(x, y);
+                        if (x % 160 == 0)
+                        {
+
+                            bmp.SetPixel(x, y, Color.Black);
+                        }
+
+
+                        if (y % 120 == 0)
+                        {
+                            bmp.SetPixel(x, y, Color.Black);
+                        }
+
+                    }
+
+                }
+
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("There was an error." +
+                    "Check the path to the image file.");
+            }
+        }
+ 
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hilo.Suspend();
+            
+            reina1.Top = 58;
+            reina1.Left = 140;
+
+            reina2.Top = 155;
+            reina2.Left = 140;
+
+            reina3.Top = 257;
+            reina3.Left = 140;
+
+            reina4.Top = 350;
+            reina4.Left = 140;
+
+            reina5.Top = 451;
+            reina5.Left = 140;
+        
+            panel.Refresh();
+            Thread.Sleep(500);
+            r1 = 140;
+            r2 = 140;
+            r3 = 140;
+            r4 = 140;
+            r5 = 140;
+            solucion = 1;
+            hilo.Resume();
+            flag = true;
+            flag2 = false;
+        }
+       
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            inicio.Enabled = true;
+            if (flag == false && flag2 == false)
+            {
+                hilo.Start();
+                flag = true;
+            }
+            else if (flag == true && flag2==false)
+            {
+                hilo.Suspend();
+                flag2 = true;
+            }
+
+            else if (flag == true && flag2 == true)
+            {
+                hilo.Resume();
+                flag2 = false;
+
+            }
+
+
+
+        }
+     
+       
+        void reinas()
+        {
+            int rr1 = 500;
+            int rr2 = 400;
+            int rr3 = 300;
+            int rr4 = 200;
+            int rr5 = 100;
+            for (r1 = 140; r1 <= 540; r1 += 100)
+            {
+                for (r2 = 140; r2 <= 540; r2 += 100)
+                {
+                    for (r3 = 140; r3 <= 540; r3 += 100)
+                    {
+                        for (r4 = 140; r4 <= 540; r4 += 100)
+                        {
+
+                            for (r5 = 140; r5 <= 540; r5 += 100)
+                            {
+                                reina5.Left = r5;
+                                reina4.Left = r4;
+                                reina3.Left = r3;
+                                reina2.Left = r2;
+                                reina1.Left = r1;
+                                if (r5 == 540 && r4 == 540 && r3 == 540 && r2 == 540 && r1 == 540)
+                                {
+                                    
+                                    MessageBox.Show("TERMINADO");
+
+                                    reina1.Top = 58;
+                                    reina1.Left = 140;
+
+                                    reina2.Top = 155;
+                                    reina2.Left = 140;
+
+                                    reina3.Top = 257;
+                                    reina3.Left = 140;
+
+                                    reina4.Top = 350;
+                                    reina4.Left = 140;
+
+                                    reina5.Top = 451;
+                                    reina5.Left = 140;
+
+                                    panel.Refresh();
+                                    r1 = 140;
+                                    r2 = 140;
+                                    r3 = 140;
+                                    r4 = 140;
+                                    r5 = 140;
+                                    solucion = 1;
+                                    flag = true;
+                                    flag2 = false;
+                                }
+
+                                if (r5 != r4 && r5 != r3 && r5 != r2 && r5 != r1 && r4 != r3 && r4 != r2 && r4 != r1 && r3 != r2 && r3 != r1 && r2 != r1)
+                                {
+
+                                    if (Math.Abs(rr5 - rr4) != Math.Abs(r5 - r4) && Math.Abs(rr5 - rr3) != Math.Abs(r5 - r3) && Math.Abs(rr5 - rr2) != Math.Abs(r5 - r2) && Math.Abs(rr5 - rr1) != Math.Abs(r5 - r1) && Math.Abs(rr4 - rr3) != Math.Abs(r4 - r3) && Math.Abs(rr4 - rr2) != Math.Abs(r4 - r2) && Math.Abs(rr4 - rr1) != Math.Abs(r4 - r1) && Math.Abs(rr3 - rr2) != Math.Abs(r3 - r2) && Math.Abs(rr3 - rr1) != Math.Abs(r3 - r1) && Math.Abs(rr2 - rr1) != Math.Abs(r2 - r1))
+                                    {
+                                        MessageBox.Show("ES SOLUCION NUMERO:  " + solucion++);
+
+                                    }
+                                    else
+                                    {
+                                       panel.Refresh();
+                                       Thread.Sleep(150);
+
+
+                                    }
+                                }
+                                else
+                                {
+                                    panel.Refresh();
+                                    Thread.Sleep(150);
+
+
+
+
+
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+       
+       
+
+
+    }
+   
+}
+
